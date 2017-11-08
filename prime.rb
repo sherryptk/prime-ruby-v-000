f40f3f57b0a12a6c2c4ca06a9ac46c1b40c12a12
Sherry <sherryptk@gmail.com>
2017-11-08 18:20:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,15 @@ def prime?(num)
 
   range = (1..num).to_a
 
-  if num <= 1 ? false
-  #   return false
-  # end
-  range.each do |i|
-
-  elsif (num % i == 0) && (i != num)
+  if num <= 1 
     return false
   end
-end
+  range.each do |i|
+
+    if (num % i == 0) && (i != num)
+      return false
+    end
+  
+  end
 true
 end
